b9e49cf9e97ca4f2b3b877a666be13b05cbc110e
izbz wh <731215820@qq.com>
2020-09-08 20:13:44 +0800

    feat: 增加zip构建命令;自动生成tgz

---
+++ dev.js
@@ -0,0 +1,26 @@
+const compressing = require('compressing')
+const fse = require('fs-extra')
+const pump = require('pump')
+const path = require('path')
+const fileList = [
+  'components', 'static', 'config.json', 'manifest.json', 'package.json',
+  'README.md', 'tsconfig.json', '.eslintignore', '.eslintrc.js', 'commitlint.config.js',
+  '.gitignore'
+]
+const join = (param) => path.join(__dirname, param)
+const compress = () => {
+  const tarStream = new compressing.tar.Stream()
+  fileList.forEach(element => {
+    tarStream.addEntry(join(`Project/${element}`))
+  })
+  const destStream = fse.createWriteStream(join('project.tgz'))
+  pump(tarStream, new compressing.gzip.FileStream(), destStream, err => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('Compress project file successfully')
+    }
+  })
+}
+compress()
+// module.exports = compress
